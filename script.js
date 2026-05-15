99f14c0e3c541dec23030d951ea446cc09de32d5
67thegoat <manvikjain.21112016@gmail.com>
2026-05-15 15:51:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
-const start = document.getElementById("start");
 const localVideo = document.getElementById("localVideo");
 
-start.onclick = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+const cameraBtn = document.getElementById("cameraBtn");
+const muteBtn = document.getElementById("muteBtn");
+const shareBtn = document.getElementById("shareBtn");
+
+const sendBtn = document.getElementById("sendBtn");
+const messageInput = document.getElementById("messageInput");
+const messages = document.getElementById("messages");
+
+let localStream;
+
+async function start(){
+  localStream = await navigator.mediaDevices.getUserMedia({
     video:true,
     audio:true
   });
 
-  localVideo.srcObject = stream;
+  localVideo.srcObject = localStream;
+}
+
+start();
+
+cameraBtn.onclick = () => {
+  const videoTrack = localStream.getVideoTracks()[0];
+
+  videoTrack.enabled = !videoTrack.enabled;
+
+  cameraBtn.innerText =
+    videoTrack.enabled ? "Camera Off" : "Camera On";
+};
+
+muteBtn.onclick = () => {
+  const audioTrack = localStream.getAudioTracks()[0];
+
+  audioTrack.enabled = !audioTrack.enabled;
+
+  muteBtn.innerText =
+    audioTrack.enabled ? "Mute" : "Unmute";
+};
+
+shareBtn.onclick = async () => {
+  const screenStream =
+    await navigator.mediaDevices.getDisplayMedia({
+      video:true
+    });
+
+  localVideo.srcObject = screenStream;
+};
+
+sendBtn.onclick = () => {
+  const text = messageInput.value;
+
+  if(text.trim() === "") return;
+
+  const div = document.createElement("div");
+
+  div.className = "message";
+  div.innerText = text;
+
+  messages.appendChild(div);
+
+  messageInput.value = "";
 };
